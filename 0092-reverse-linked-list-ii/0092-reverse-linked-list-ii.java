a4965f4e1aea5ed8409cad779296750a9cea9f39
sarthak kumar <114852200+45Sarthak@users.noreply.github.com>
2026-08-01 21:22:19 +0530

    Time: 0 ms (100%), Space: 43 MB (67.76%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -14,40 +14,29 @@
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
 
-        if (head == null || left == right) {
-            return head;
+    if(head==null || head.next==null){
+        return head;
+    }
+
+    ListNode dummy=new ListNode(0);
+    dummy.next=head;
+
+
+
+        ListNode prev=dummy;
+        for(int i=1;i<left;i++){
+            prev=prev.next;
         }
 
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-
-        // beforeLeft ko left-1 position tak le jao
-        ListNode beforeLeft = dummy;
-        for (int i = 1; i < left; i++) {
-            beforeLeft = beforeLeft.next;
+        ListNode curr=prev.next;
+        for(int i=1;i<right-left+1;i++){
+            ListNode temp=prev.next;
+            prev.next=curr.next;
+            curr.next=curr.next.next;
+            prev.next.next=temp;
         }
-
-        // Reverse start
-        ListNode leftNode = beforeLeft.next;
-        ListNode prev = null;
-        ListNode curr = leftNode;
-
-        // LC206 logic
-        for (int i = 0; i < right - left + 1; i++) {
-
-            ListNode next = curr.next;
-
-            curr.next = prev;
-
-            prev = curr;
-
-            curr = next;
-        }
-
-        // Reconnect
-        beforeLeft.next = prev;
-        leftNode.next = curr;
 
         return dummy.next;
+        
     }
 }
