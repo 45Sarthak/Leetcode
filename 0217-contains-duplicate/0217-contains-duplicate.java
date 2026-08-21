c808e4a783e329a2faffcaefd40afb2f6477856e
sarthak kumar <114852200+45Sarthak@users.noreply.github.com>
2026-08-22 02:02:27 +0530

    Time: 39 ms (5.1%), Space: 110.5 MB (9.47%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,18 @@
 import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        boolean dupli=false;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                dupli=true;
+        HashMap<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i]) > 1){
+                return true;
             }
         }
-        return dupli;
+
+        return false;
     }
 }
